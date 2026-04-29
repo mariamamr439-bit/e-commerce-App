d5e5d49c82fcc865b6c04d64dae9ce03077b6aca
mariamamr439-bit <mariamamr439@gmail.com>
2026-04-29 14:03:32 +0300

    First commit

--- src/app/features/not-found/not-found.component.ts
+++ src/app/features/not-found/not-found.component.ts
@@ -1,9 +1,22 @@
-import { Component } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-not-found',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './not-found.component.html',
   styleUrl: './not-found.component.css',
 })
-export class NotFoundComponent {}
+export class NotFoundComponent {
+  private location = inject(Location);
+  private router = inject(Router);
+
+  goBack(): void {
+    if (window.history.length > 1) {
+      this.location.back();
+    } else {
+      this.router.navigate(['/']);
+    }
+  }
+}
